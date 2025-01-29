ede398ab16c486b72db557100985443cb76a30e7
Arthur_bhs <arthur.shekinarfoxy@gmail.com>
2025-01-29 17:21:30 -0300

    erros de deploy corrigidos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, IconButton, Box, Menu, MenuItem } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Box, Menu, MenuItem } from "@mui/material";
 import { Menu as MenuIcon } from "@mui/icons-material";
 import { styled } from "@mui/system";
 import { Link } from "react-router-dom";
